70c3ae350fe5c968486507b4aa0230cc2b899d02
unniznd <au6695@gmail.com>
2022-07-06 21:27:05 +0530

    minor updates in main.c

--- 8/main.c
+++ 8/main.c
@@ -19,9 +19,4 @@ int main()
     read(0,buff,100);
     strcpy(shared_memory,buff); 
     printf("You wrote : %s\n",(char *)shared_memory);  
-    shmid = shmget((key_t)2345, 1024, 0666);  
-    printf("Key of shared memory is %d\n",shmid);  
-    shared_memory=shmat(shmid,NULL,0);
-    printf("Process attached at %p\n",shared_memory);  
-    printf("Data read from shared memory is : %s\n",(char *)shared_memory);  
 }  
